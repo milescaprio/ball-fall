61e7816c45787824827789f59a6690ed55445abb
PunkyMunky64 <46407361+PunkyMunky64@users.noreply.github.com>
2022-11-20 22:46:20 -0800

    adding more abstractions for gl'

--- src/gl.rs
+++ src/gl.rs
@@ -18,58 +18,81 @@ use piston::event_loop::{EventSettings, Events, EventLoop};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 
-static opengl : OpenGL = OpenGL::V4_0;
+type RenderFunction = Box<dyn FnMut(u64, u64, graphics::Context, &mut GlGraphics)>;
+type UpdateFunction = Box<dyn FnMut(u64)>;
+static OPENGL_VER : OpenGL = OpenGL::V4_0;
 
 pub struct Window {
     width : usize,
     height : usize,
     gl : GlGraphics,
-    window : GlutinWindow,
+    window : Option<GlutinWindow>,
     events : Events,
     rtick : u64,
     utick : u64,
-    renderf : Box<dyn Fn(u64)>,
-    updatef : Box<dyn Fn(u64, graphics::Context, GlGraphics)>,
+    renderf : Option<Box<dyn FnMut(u64, u64, graphics::Context, &mut GlGraphics)>>,
+    updatef : Option<Box<dyn FnMut(u64)>>,
 }
 
 impl Window {
+    
+    const FLAGLESS : u32 = 0;
+    const EXIT_ON_ESC : u32 = 0x1;
+    const RESIZABLE : u32 = 0x2;
+    const VSYNC : u32 = 0x4;
+    const DECORATED : u32 = 0x8;
+    const FULLSCREEN : u32 = 0x16;
+    const SRGB : u32 = 0x32;
+    const CONTROLLERS : u32 = 0x64;
+    const DEFAULT_FLAGS : u32 = Self::EXIT_ON_ESC | Self::RESIZABLE | Self::VSYNC | Self::DECORATED;
+
     pub fn new(width : usize, height : usize) -> Window {
+        //Create a new window
         let mut events = Events::new(EventSettings::new());
         //events.set_max_fps(165);
-        events.set_lazy(true);
-        let mut window: GlutinWindow = WindowSettings::new(
-            "Test Window!",
-            [width as u32, height as u32]
-        ).opengl(opengl)
-            .exit_on_esc(true)
-            .build()
-            .unwrap();
-        let renderf = Box::new(|c, gl| {
-            let COOL_COLOR : [f32; 4] = [0.65, 0.85, 0.13, 1.0];
-            let COOL_COLOR2 : [f32; 4] = [0.35, 0.15, 0.87, 1.0];
-            // if self.tick != 0 {
-            graphics::clear(COOL_COLOR, gl);
-            let mut rect = graphics::rectangle::square(0.0, 0.0, (self.rtick % 1024) as f64);
-            graphics::rectangle(COOL_COLOR2, rect, c.transform, gl)
-            // }
-        });
-        let updatef = Box::new(|self.rtick|);
-        Window {
+        //events.set_lazy(true);
+        Self {
             width,
             height,
-            gl : GlGraphics::new(opengl),
-            window,
+            gl : GlGraphics::new(OPENGL_VER),
+            window : None,
             events,
             rtick : 0,
             utick : 0,
-            renderf,
-            updatef,
+            renderf : None,
+            updatef : None,
         }
     }
+    pub fn begin(&mut self, title : String, FLAGS : u32) {
+        self.window = WindowSettings::new(
+            "",
+            [self.width as u32, self.height as u32],
+        ).opengl(OPENGL_VER)
+            .resizable(FLAGS | Self::RESIZABLE != 0)
+            .exit_on_esc(FLAGS | Self::EXIT_ON_ESC != 0)
+            .vsync(FLAGS | Self::VSYNC != 0)
+            .title(title)
+            .fullscreen(FLAGS | Self::FULLSCREEN != 0)
+            .decorated(FLAGS | Self::DECORATED != 0)
+            .controllers(FLAGS | Self::CONTROLLERS != 0)
+            .srgb(FLAGS | Self::SRGB != 0)
+            .build().unwrap();
+    }   
+    pub fn init(width : usize, height : usize, renderf : RenderFunction, updatef : UpdateFunction) -> Window {
+        let mut ret = Window::new(width, height);
+        ret.renderf = Some(renderf);
+        ret.updatef = Some(updatef);
+        ret
+    }
     pub fn maintain(&mut self) {
-        while let Some(e) = self.events.next(&mut self.window) {
+        while let Some(e) = self.events.next(&mut self.window.unwrap_or_else(|| panic!("Window not initialized!"))) {
+            //dbg!(&e);
             if let Some(r) = e.render_args() {
                 self.render(&r);
+            }
+            if let Some(u) = e.update_args() {
+                self.update(&u);
+                //println!("Update tick: {}", self.utick);
             }
         }
     }
@@ -78,13 +101,41 @@ impl Window {
     }
     pub fn render(&mut self, args : &RenderArgs) {
         use graphics;
-        self.gl.draw(args.viewport(), self.renderf);
-        self.tick += 1;
+        if let Some(f) = &mut self.renderf {
+            self.gl.draw(args.viewport(), |c, gl| {f(self.rtick, self.utick, c, gl)});
+        }
+        self.rtick += 1;
     }
     pub fn update(&mut self, args : &UpdateArgs) {
         use graphics;
-        self.updatef();
+        if let Some(f) = &mut self.updatef {
+            f(self.utick);
+        }
         self.utick += 1;
+    }
+    pub fn set_render_fn(&mut self, new_renderf : Box<dyn FnMut(u64, u64, graphics::Context, &mut GlGraphics)>) {
+        self.renderf = Some(new_renderf);
+    }
+    pub fn set_update_fn(&mut self, new_updatef : Box<dyn FnMut(u64)>) {
+        self.updatef = Some(new_updatef);
+    }
+}
+
+impl Default for Window {
+    fn default() -> Self {
+        let renderf = |rtick : u64, utick : u64, c : graphics::Context, gl : &mut GlGraphics| {
+            const COOL_COLOR : [f32; 4] = [0.65, 0.85, 0.13, 1.0];
+            const COOL_COLOR2 : [f32; 4] = [0.35, 0.15, 0.87, 1.0];
+            // if self.tick != 0 {
+            graphics::clear(COOL_COLOR, gl);
+            let mut rect = graphics::rectangle::square(0.0, 0.0, ((rtick + utick) % 1024) as f64);
+            graphics::rectangle(COOL_COLOR2, rect, c.transform, gl)
+            // }
+        };
+        let updatef = |utick : u64|{
+            
+        };
+        Self::init(1024, 768, Box::new(renderf), Box::new(updatef))
     }
 }
 
@@ -93,8 +144,8 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        let mut my_window = Window::new(800,600);
-        my_window.set_ups(100);
+        let mut my_window = Window::default();
+        my_window.begin("Yeet".to_string(), Window::DEFAULT_FLAGS);
         loop {
             my_window.maintain();
         }
